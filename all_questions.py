20fc19006ffa58c3419f620e33f1d3a99965ac36
SAMEERA-ROMPICHERLA <156724151+SAMEERA-ROMPICHERLA@users.noreply.github.com>
2024-03-31 14:17:18 -0400

    Add files via upload

---
+++ all_questions.py
@@ -0,0 +1,195 @@
+# Add import files
+import pickle
+
+
+
+# -----------------------------------------------------------
+def question1():
+    answers = {}
+
+    # string "yes" or "no"
+    answers["(a)"] = "no"
+    answers["(b)"] = "no"
+    answers["(c)"] = "yes"
+    answers["(d)"] = "yes"
+
+    # explain-string: explanation in english prose
+    answers["(a) explain"] = "Rules are not mutually exclusive because more than one rule can apply to a set of attributes simultaneously."
+    answers["(b) explain"] = "The rule set is not exhaustive as there are attribute combinations not covered by any rule."
+    answers["(c) explain"] = "Ordering is needed because the application of some rules before others can affect the classification outcome."
+    answers["(d) explain"] = "A default class is necessary for handling attribute combinations that do not match any of the provided rules."
+
+    return answers
+
+
+# -----------------------------------------------------------
+"""
+def question2():
+    answers = {}
+
+    # string "yes" or "no"
+    answers["(a)"] = None
+    answers["(b)"] = None
+    answers["(c)"] = None
+    answers["(d)"] = None
+
+    # explain_string: explanation in english prose
+    answers["(a) explain"] = None
+    answers["(b) explain"] = None
+    answers["(c) explain"] = None
+    answers["(d) explain"] = None
+
+    return answers
+"""
+# -----------------------------------------------------------
+def question3():
+    answers = {}
+
+    # string "yes" or "no"
+    answers["(a)"] = "yes"
+    answers["(b)"] = "no"
+    answers["(c)"] = "no"
+
+    # explain-string: explanation in english prose
+    answers["(a) example"] = "Each rule specifies a unique combination of attributes, making them mutually exclusive."
+    answers["(b) example"] = "Not all possible combinations of attributes are covered, indicating the rules are not exhaustive."
+    answers["(c) example"] = "Since the rules are mutually exclusive, their order of application does not change the outcome."
+
+
+    return answers
+# -----------------------------------------------------------
+def question7():
+    answers = {}
+
+    # bool: True/False
+    answers["(a)"] = True
+    answers["(b)"] = True
+    answers["(c)"] = False
+    answers["(d)"] = False
+
+    # explain_string: explanation in english prose
+    answers["(a) explain"] = "In back-propagation, the error gradient for each layer is computed based on the gradient of the subsequent layer, utilizing the chain rule."
+    answers["(b) explain"] = "The activations at the (k+1)th layer are computed using the activations at the kth layer through forward propagation."
+    answers["(c) explain"] = "The vanishing gradient problem refers to gradients becoming small as error is propagated back, not training errors vanishing to zero."
+    answers["(d) explain"] = "Even if the ANN model classifies all training instances correctly, gradients of loss with respect to weights could still be nonzero."
+
+    return answers
+
+# -----------------------------------------------------------
+def question8():
+    answers = {}
+
+    # float
+    answers["(a) P(X_1=1)"] = 0.65
+    answers["(a) P(X_2=1)"] = 0.41
+    answers["(a) P(X_1=1,X_2=1)"] = 0.28
+
+    # string: "dependent" or "independent"
+    answers["(a) Relationship between X_1 and X_2"] = "dependent"
+
+    # string: "yes" or "no"
+    answers["(b) X_1 and X_2 conditionally independent given the class?"] = "yes"
+
+    # float
+    answers["(c) P(X_1=1 | +)"] = 0.8
+    answers["(c) P(X_1=1 | -)"] = 0.5
+    answers["(c) P(X_2=1 | +)"] = 0.5
+    answers["(c) P(X_2=1 | -)"] = 0.32
+    answers["(c) P(X_3=1 | +)"] = 0.4
+    answers["(c) P(X_3=1 | -)"] = 0.16
+
+    # For each row give the class predicted by the model after training using Naive Bayes
+    # String: either '+' or '-'
+    answers["(d) Row 1"] = "+"
+    answers["(d) Row 2"] = "-"
+    answers["(d) Row 3"] = "-"
+    answers["(d) Row 4"] = "-"
+
+    # float between 0 and 1
+    answers["(d) Training error rate"] = 0.38
+
+    return answers
+
+# --------------------------------------------------------
+def question9():
+    answers = {}
+
+    # int
+    answers["(a) K"] = 5
+    answers["(b) K"] = 5
+
+    # explain_string
+    answers["(a) explain"] = None
+    answers["(b) explain"] = None
+
+    return answers
+
+# --------------------------------------------------------
+def question10():
+    answers = {}
+
+    # float
+    answers["(a) P(A=1|+)"] = 0.6
+    answers["(a) P(B=1|+)"] = 0.4
+    answers["(a) P(C=1|+)"] = 0.8
+    answers["(a) P(A=1|-)"] = 0.4
+    answers["(a) P(B=1|-)"] = 0.4
+    answers["(a) P(C=1|-)"] = 0.2
+
+    # type: explanatory string
+    answers["(a) P(A=1|+) explain your answer"] = "if most of the time when A is 1, the outcome is positive, then we think A=1 makes a positive outcome likely."
+  
+    # type: float
+    # note: R is the sample (A=1,B=1,C=1)
+    answers["(b) P(+|R)"] = 1.0
+    answers["(b) P(R|+)"] = 0.2
+    answers["(b) P(R|-)"] = 0.0
+
+    # string, '+' or '-'
+    answers["(b) class label"] = "+"
+
+    # explain_string
+    answers["(b) Explain your reasoning"] = "To guess if something is positive or negative, we use what we learned from counting and see which outcome looks more likely based on our counts."
+  
+    # float
+    answers["(c) P(A=1)"] = 0.5
+    answers["(c) P(B=1)"] = 0.4
+    answers["(c) P(A=1,B=1)"] = 0.2
+
+    # type: string, 'yes' or 'no'
+    answers["(c) A independent of B?"] = "yes"
+  
+    # type: float
+    answers["(d) P(A=1)"] = 0.5
+    answers["(d) P(B=0)"] = 0.6
+    answers["(d) P(A=1,B=0)"] = 0.3
+
+    # type: string: 'yes' or 'no'
+    answers["(d) A independent of B?"] = "yes"
+  
+    # type: float
+    answers["(e) P(A=1,B=1|+)"] = 0.2
+    answers["(e) P(A=1|+)"] = 0.6
+    answers["(e) P(B=1|+)"] = 0.4
+
+    # type: string: 'yes' or 'no'
+    answers["(e) A independent of B given class +?"] = "no"
+
+    # type: explanatory string
+    answers["(e) A and B conditionally independent given class +, explain"] =  "We look again with our counts, but this time we check if knowing the outcome ahead of time changes how we think A and B are related. If not, then A and B don’t give us any new clues when we already know the outcome."
+  
+    return answers
+# --------------------------------------------------------
+if __name__ == '__main__':
+    answers_dict = {}
+    answers_dict['question1'] = question1()
+    answers_dict['question2'] = question2()
+    answers_dict['question3'] = question3()
+    answers_dict['question4'] = question4()
+    answers_dict['question7'] = question7()
+    answers_dict['question8'] = question8()
+    answers_dict['question9'] = question9()
+    answers_dict['question10'] = question10()
+
+    with open('answers.pkl', 'wb') as f:
+        pickle.dump(answers_dict, f)
